9843e697fbd6640e75252c9559f0810550829e23
Denis.S <denis.s@MacBook-Pro-3.local>
2022-10-14 20:34:41 +0300

    added index.js

---
+++ index.js
@@ -0,0 +1,3 @@
+exports.itemFilterJson = require('./filters/totem-item.json')
+exports.avatarFilterJson = require('./filters/totem-avatar.json')
+exports.assets_abi = require('./ABI/assets_abi.json')
