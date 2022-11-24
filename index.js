723d333f61b8cd0181c0f0b0a79a3b689e1120ac
illiadimension <114153017+illiadimension@users.noreply.github.com>
2022-11-24 15:42:22 +0200

    add export filters for dreadStone and monkVsRobots

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ exports.avatarFilterJson = require('./filters/totem-avatar.json')
 exports.assets_abi = require('./ABI/assets_abi.json')
 exports.game_directory_abi = require('./ABI/json/TotemGameDirectory.json')
 exports.legacy_abi = require('./ABI/json/TotemLegacy.json')
+exports.totemAvatarDreadstoneKeepFilterJson = require('./filters/totem-avatar-dreadstone-keep.json')
+exports.monkVsRobotsAvatarFilterJson = require('./filters/monk-vs-robots-avatar.json')
+
